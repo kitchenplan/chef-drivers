ea60eeb879a5e6c85a8904a6f45c8baca4400f5b
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-03-04 15:00:03 +0100

    Unshare printers

--- providers/printers.rb
+++ providers/printers.rb
@@ -11,6 +11,9 @@ action :install do
         execute "Install #{new_resource.name}" do
             command   %{lpadmin -p "#{new_resource.name}" -L "#{new_resource.location}" -E -v lpd://#{new_resource.ip} -P "/opt/kitchenplan/vendor/cookbooks/drivers/templates/default/#{new_resource.driver}"}
         end
+        execute "Unshare #{new_resource.name}" do
+            command   %{lpadmin -p "#{new_resource.name}" -o printer-is-shared=false}
+        end
      new_resource.updated_by_last_action(true)
     end
 end
