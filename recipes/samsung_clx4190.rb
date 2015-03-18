9e5eb0f61bfa86e0e4a0e004bd7247299bdc3ea9
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-03-18 13:42:47 +0100

    Fix quotes

--- recipes/samsung_clx4190.rb
+++ recipes/samsung_clx4190.rb
@@ -6,6 +6,6 @@ end
 node['drivers']['samsung_clx4190'].each do |samsung|
   execute "Install Samsung printer: #{samsung['name']}" do
       command "lpadmin -p '#{samsung['name']}' -L '#{samsung['location']}' -E -v lpd://#{samsung['ip']} -P \"#{Chef::Config[:file_cache_path]}/Samsung_CLX_4190.ppd\" -o printer-is-shared=false"
-      not_if 'lpstat -v | grep -q "#{samsung['name']}"'
+      not_if "lpstat -v | grep -q '#{samsung['name']}'"
   end
 end
