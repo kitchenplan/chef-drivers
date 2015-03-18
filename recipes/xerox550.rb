7923179c282148ba6ace8a8416ff145823b8507d
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-03-18 15:32:29 +0100

    More fixes

--- recipes/xerox550.rb
+++ recipes/xerox550.rb
@@ -1,13 +1,13 @@
 remote_file "#{Chef::Config[:file_cache_path]}/FIT224989_mac10.10_.zip" do
     source "http://download.support.xerox.com/pub/drivers/550_560_DCP/drivers/macosx1010/pt_BR/FIT224989_mac10.10_.zip"
     owner node['current_user']
-    not_if ::File.exists?("#{Chef::Config[:file_cache_path]}/FIT224989_mac10.10_.zip")
+    not_if do ::File.exists?("#{Chef::Config[:file_cache_path]}/FIT224989_mac10.10_.zip") end
 end
 
 execute "unzip the xerox550 driver" do
     command "unzip -j -u #{Chef::Config[:file_cache_path]}/FIT224989_mac10.10_.zip -d #{Chef::Config[:file_cache_path]}/"
     user node['current_user']
-    not_if ::File.exists?("#{Chef::Config[:file_cache_path]}/XC_Integrated550_560_v1_1R_EFIGSBpDX_FD47_v1.dmg")
+    not_if do ::File.exists?("#{Chef::Config[:file_cache_path]}/XC_Integrated550_560_v1_1R_EFIGSBpDX_FD47_v1.dmg") end
 end
 
 dmg_package 'Fiery Printer Driver' do
