5d4aec161a16a5051a80f8e9e256b7061d9d88de
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-03-18 13:08:19 +0100

    Update samsung_ml3470.rb

--- recipes/samsung_ml3470.rb
+++ recipes/samsung_ml3470.rb
@@ -3,7 +3,7 @@ cookbook_file "Samsung ML-3470 Series.gz" do
   action :create_if_missing
 end
 
-node['driver']['samsung_ml3470'].each do |samsung|
+node['drivers']['samsung_ml3470'].each do |samsung|
   execute 'Install Samsung printer: #{samsung['name']}' do
       command "lpadmin -p '#{samsung['name']}' -L '#{samsung['location']}' -E -v lpd://#{samsung['ip']} -P \"#{Chef::Config[:file_cache_path]}/Samsung ML-3470 Series.gz\" -o printer-is-shared=false"      
       not_if 'lpstat -v | grep -q "#{samsung['name']}"'
